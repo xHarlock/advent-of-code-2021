0326a96ec401857e1abfaa97a7bf515eab7a2a8d
xHarlock <72373341+xHarlock@users.noreply.github.com>
2021-12-07 11:28:17 +0100

    Day 7

---
+++ src/day_07/Day_07.java
@@ -0,0 +1,52 @@
+package day_07;
+
+import java.util.Arrays;
+
+import utils.AdventDay;
+import utils.Reader;
+
+public class Day_07 extends AdventDay {
+
+	public static void main(String[] args) {		
+		int[] input = convert(Reader.getInput(7, 2021)[0].split(","));
+
+		System.out.println(part_1(input));
+		System.out.println(part_2(input));
+	}
+
+	public static int part_1(int[] input) {
+		int[] array = new int[input.length];
+		int max_pos = Arrays.stream(input).max().getAsInt();
+		int cheapest = Integer.MAX_VALUE;
+
+		for (int i = 0; i < max_pos; i++) {
+			for (int j = 0; j < input.length; j++)
+				array[j] = Math.abs(i - input[j]);
+
+			int fuel = Arrays.stream(array).sum();
+
+			if (fuel < cheapest)
+				cheapest = fuel;
+		}
+		return cheapest;
+	}
+
+	public static int part_2(int[] input) {
+		int[] array = new int[input.length];
+		int max_pos = Arrays.stream(input).max().getAsInt();
+		int cheapest = Integer.MAX_VALUE;
+
+		for (int i = 0; i < max_pos; i++) {
+			for (int j = 0; j < input.length; j++) {
+				int cost = Math.abs(i - input[j]);
+				array[j] = cost * (cost + 1) / 2;
+			}
+
+			int fuel = Arrays.stream(array).sum();
+
+			if (fuel < cheapest)
+				cheapest = fuel;
+		}
+		return cheapest;
+	}
+}
