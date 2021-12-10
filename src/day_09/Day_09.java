ee65a04b4cf96cdd6b75bccc388b8fa2c85581db
xHarlock <72373341+xHarlock@users.noreply.github.com>
2021-12-10 15:42:51 +0100

    Cleaned up day 9

--- src/day_09/Day_09.java
+++ src/day_09/Day_09.java
@@ -6,128 +6,91 @@ import java.util.List;
 
 import utils.AdventDay;
 
-
-// TODO Clean up this mess
-
-
 public class Day_09 extends AdventDay {
 
-	private String[] input;
+	private Node[][] grid;
 
 	@Override
 	public long part_1() {
-		int[][] grid = toGrid(input);
-
-		int danger_levels = 0;
-
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[i].length; j++) {
-				// Top row
-				if (i == 0) {
-					// Leftmost col
-					if (j == 0) {
-						if (grid[i][j] < grid[i + 1][j] && grid[i][j] < grid[i][j + 1])
-							danger_levels += grid[i][j] + 1;
-					}
-
-					// Rightmost col
-					else if (j == grid[i].length - 1) {
-						if (grid[i][j] < grid[i + 1][j] && grid[i][j] < grid[i][j - 1])
-							danger_levels += grid[i][j] + 1;
-					}
-
-					// Else
-					else {
-						if (grid[i][j] < grid[i + 1][j] && grid[i][j] < grid[i][j + 1] && grid[i][j] < grid[i][j - 1])
-							danger_levels += grid[i][j] + 1;
-					}
-				}
-
-				// Bottom row
-				else if (i == grid.length - 1) {
-					// Leftmost col
-					if (j == 0) {
-						if (grid[i][j] < grid[i - 1][j] && grid[i][j] < grid[i][j + 1])
-							danger_levels += grid[i][j] + 1;
-					}
-
-					// Rightmost col
-					else if (j == grid[i].length - 1) {
-						if (grid[i][j] < grid[i - 1][j] && grid[i][j] < grid[i][j - 1])
-							danger_levels += grid[i][j] + 1;
-					}
-
-					// Else
-					else {
-						if (grid[i][j] < grid[i - 1][j] && grid[i][j] < grid[i][j + 1] && grid[i][j] < grid[i][j - 1])
-							danger_levels += grid[i][j] + 1;
-					}
-				}
-
-				// Middle
-				else {
-					// Leftmost col
-					if (j == 0) {
-						if (grid[i][j] < grid[i + 1][j] && grid[i][j] < grid[i - 1][j] && grid[i][j] < grid[i][j + 1])
-							danger_levels += grid[i][j] + 1;
-					}
-
-					// Rightmost col
-					else if (j == grid[i].length - 1) {
-						if (grid[i][j] < grid[i + 1][j] && grid[i][j] < grid[i - 1][j] && grid[i][j] < grid[i][j - 1])
-							danger_levels += grid[i][j] + 1;
-					}
-
-					// Else
-					else {
-						if (grid[i][j] < grid[i + 1][j] && grid[i][j] < grid[i - 1][j] && grid[i][j] < grid[i][j + 1]
-								&& grid[i][j] < grid[i][j - 1])
-							danger_levels += grid[i][j] + 1;
-					}
-				}
+		long danger_levels = 0;
+		for (Node[] row : grid) {
+			for (Node node : row) {
+				if (node.isLowPoint())
+					danger_levels += 1 + node.height;
 			}
 		}
-
 		return danger_levels;
-	}
-
-	@Override
-	public long part_2() {
-		int[][] grid = toGrid(input);
-
-		List<Long> sizes = new ArrayList<>();
-
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[i].length; j++) {
-				if (grid[i][j] != 9 && grid[i][j] != -1)
-					sizes.add(getBasinSize(j, i, grid));
-			}
-		}
-
-		Collections.sort(sizes);
-		return sizes.get(sizes.size() - 3) * sizes.get(sizes.size() - 2) * sizes.get(sizes.size() - 1);
 	}
 	
 	@Override
-	public void initialize(String[] input) {
-		this.input = input;
+	public long part_2() {
+		List<Long> sizes = new ArrayList<>();		
+		for (Node[] row : grid) {
+			for (Node node : row) {
+				if (node.height != 9 && node.height != -1)
+					sizes.add(getBasinSize(node));
+			}
+		}		
+		Collections.sort(sizes);
+		return sizes.get(sizes.size() - 3) * sizes.get(sizes.size() - 2) * sizes.get(sizes.size() - 1);
 	}
 
-	/**
-	 * Counts surrounding points of the same basin
-	 */
-	public static long getBasinSize(int x, int y, int[][] grid) {
-		// Base cases
-		if (y < 0 || y >= grid.length)
-			return 0;
-		if (x < 0 || x >= grid[0].length)
-			return 0;
-		if (grid[y][x] == -1 || grid[y][x] == 9)
-			return 0;
+	@Override
+	public void initialize(String[] input) {
+		int[][] raw_grid = toGrid(input);
+		this.grid = new Node[raw_grid.length][raw_grid[0].length];
 
-		grid[y][x] = -1;
+		// Convert elements of the grid into nodes and connects them
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				Node node = new Node(j, i, raw_grid[i][j]);
+				grid[i][j] = node;
+				// Top
+				if (i > 0) {
+					node.neighbours.add(grid[i - 1][j]);
+					grid[i - 1][j].neighbours.add(node);
+				}
+				// Left
+				if (j > 0) {
+					node.neighbours.add(grid[i][j - 1]);
+					grid[i][j - 1].neighbours.add(node);
+				}
+			}
+		}
+	}
 
-		return 1 + getBasinSize(x - 1, y, grid) + getBasinSize(x + 1, y, grid) + getBasinSize(x, y - 1, grid)
-				+ getBasinSize(x, y + 1, grid);
+	/** Counts surrounding points of the same basin */	
+	public long getBasinSize(Node node) {
+		// Base case
+		if (node.height == -1 || node.height == 9)
+			return 0;
+		
+		node.height = -1;
+		
+		long size = 0;		
+		for (Node n : node.neighbours)
+			size += getBasinSize(n);
+		return 1 + size;
+	}
+}
+
+class Node {
+	int x, y;
+	int height;
+	List<Node> neighbours;
+
+	public Node(int x, int y, int height) {
+		this.x = x;
+		this.y = y;
+		this.height = height;
+		this.neighbours = new ArrayList<>();
+	}
+
+	boolean isLowPoint() {
+		for (Node node : neighbours) {
+			if (node.height <= this.height)
+				return false;
+		}
+		return true;
 	}
 }
